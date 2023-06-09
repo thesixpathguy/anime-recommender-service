9be346809db18259e1cd6688361ff260c0f0ca8b
thesixpathguy <thesixpathguy@gmail.com>
2023-06-09 23:36:36 +0530

    added try catch blocks

--- services/emailSender/emailSender.js
+++ services/emailSender/emailSender.js
@@ -2,22 +2,26 @@ const nodemailer = require("nodemailer");
 const { logEvents } = require("../../middleware/logEvents");
 
 const sendEmail = async (from, pass, to, subject, text, html) => {
-  let transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: from,
-      pass: pass,
-    },
-  });
-  let info = await transporter.sendMail({
-    from: "test@test.com",
-    to: to,
-    subject: subject,
-    text: text,
-    html: html,
-  });
-  console.log("Message sent: %s", info.messageId);
-  logEvents(`${info.messageId}\t${info.accepted}`, "emailLog.txt");
+  try {
+    let transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: from,
+        pass: pass,
+      },
+    });
+    let info = await transporter.sendMail({
+      from: "test@test.com",
+      to: to,
+      subject: subject,
+      text: text,
+      html: html,
+    });
+    console.log("Message sent: %s", info.messageId);
+    logEvents(`${info.messageId}\t${info.accepted}`, "emailLog.txt");
+  } catch (err) {
+    return err;
+  }
 };
 
 module.exports = sendEmail;
