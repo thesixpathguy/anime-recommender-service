6ccb4b297984805b8c966e3780f49503183f04b6
thesixpathguy <thesixpathguy@gmail.com>
2023-06-09 22:38:51 +0530

    added comments

--- controllers/shootEmailController.js
+++ controllers/shootEmailController.js
@@ -1,5 +1,10 @@
 const asyncHandler = require("express-async-handler");
 
+/*
+    @desc shoots email with anime content
+    @route GET /api/shootEmail
+    @access public
+*/
 const shootEmail = asyncHandler(async (req, res) => {
   res.json({ esgrlm: "rsmngo" });
 });
