0c1e12ad9aad5ad6387d103f4a3e6658af27ce5b
thesixpathguy <thesixpathguy@gmail.com>
2023-06-09 17:56:32 +0530

    resolved bugs

--- services/fetcher/characterImageFetcher.js
+++ services/fetcher/characterImageFetcher.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 
 const config = {
   method: "GET",
-  url: "https://api.jikan.moe/v4/anime",
+  url: "https://api.jikan.moe/v4/characters",
   params: {
     q: "",
     sort: "asc",
@@ -10,19 +10,20 @@ const config = {
   },
 };
 
-const dummyAnimeURI = "https://cdn.myanimelist.net/images/anime/7/42453.jpg";
+const dummyCharacterURI =
+  "https://cdn.myanimelist.net/images/characters/5/223525.jpg";
 
-const fetchAnimeImage = async (animeName) => {
+const fetchCharacterImage = async (characterName) => {
   try {
-    config.params.q = animeName;
-    config.params.letter = animeName;
+    config.params.q = characterName;
+    config.params.letter = characterName;
     const response = await axios.request(config);
     if (response?.data?.data[0]?.images?.jpg?.image_url) {
       return response.data.data[0].images.jpg.image_url;
-    } else return dummyAnimeURI;
+    } else return dummyCharacterURI;
   } catch (err) {
     return err;
   }
 };
 
-module.exports = fetchAnimeImage;
+module.exports = fetchCharacterImage;
--- services/fetcher/randomQuoteFetcher.js
+++ services/fetcher/randomQuoteFetcher.js
@@ -1,5 +1,6 @@
 const axios = require("axios");
 const fetchAnimeImage = require("./animeImageFetcher");
+const fetchCharacterImage = require("./characterImageFetcher");
 
 const config = {
   method: "GET",
@@ -9,9 +10,17 @@ const config = {
 const fetchRandomQuote = async () => {
   try {
     const response = await axios.request(config);
-    // console.log(response.data);
-    const imageURI = await fetchAnimeImage(response.data.anime || "");
-    const quote = { ...response.data, imageURI };
+
+    // fetching anime image
+    let animeImageURI = await fetchAnimeImage(response.data.anime || "");
+    console.log(typeof animeImageURI !== "undefined");
+
+    // fetching character image
+    let characterImageURI = await fetchCharacterImage(
+      response.data.character || ""
+    );
+
+    const quote = { ...response.data, animeImageURI, characterImageURI };
     delete quote["key"]; // deleting redundant params
     delete quote["__v"]; // deleting redundant params
     return quote;
