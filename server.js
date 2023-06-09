2531ff46d7c959eed56ce2c82ac9681d858d714d
Kartik Upadhyay <37153644+AaronStone24@users.noreply.github.com>
2023-06-09 17:15:09 +0530

    Added Subscribe webpage (#5)
    Added the subscribe webpage and made changes to server.js to allow for running without connecting the DB

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const path = require("path");
 const corsOptions = require("./config/corsOptions");
 const credentials = require("./middleware/credentials");
 const mongoose = require("mongoose");
@@ -9,26 +10,33 @@ const connectDB = require("./config/dbConn");
 const errorHandler = require("./middleware/errorHandler.js");
 
 const PORT = process.env.PORT || 3500;
+const env = process.env.NODE_ENV || "dev";
 
 // connecting DB
-connectDB();
+if (env !== "dev")
+    connectDB();
 
 // Cors settings
 app.use(credentials);
 app.use(cors(corsOptions));
 
-// for form data (encoded data)
-app.use(express.urlencoded({ extended: false }));
-
-// for json data
-app.use(express.json());
+// Middleware
+app.use(express.urlencoded({ extended: false })); // for form data (encoded data)
+app.use(express.json()); // for json data
+app.use('/', express.static(path.join(__dirname, 'public')))
 
 //routes
+app.use("/", require('./routes/root.js'));
 
 // error handler
 app.use(errorHandler);
 
-mongoose.connection.once("open", () => {
-  console.log("Connected to mongoDB");
-  app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
-});
+if (env !== 'dev'){
+    mongoose.connection.once("open", () => {
+      console.log("Connected to mongoDB");
+      app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
+    });
+}
+else {
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
+}
