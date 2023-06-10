38c14752f99ce1783d751596461734df7a7e5d3b
thesixpathguy <thesixpathguy@gmail.com>
2023-06-10 14:00:52 +0530

    added prod and dev env config

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const envConfig = require("./config/envConfig");
 const cors = require("cors");
 const path = require("path");
 const corsOptions = require("./config/corsOptions");
@@ -11,16 +12,16 @@ const connectDB = require("./config/dbConn");
 const errorHandler = require("./middleware/errorHandler");
 const cronJob = require("./services/cronScheduler/cronScheduler");
 
-const PORT = process.env.PORT || 3500;
-const env = process.env.NODE_ENV || "dev";
+const PORT = envConfig.httpPORT || 3500;
 
 // running cron job
 cronJob.start();
 
 // connecting DB
-if (env !== "dev") connectDB();
+console.log(envConfig.envName);
+if (envConfig.envName !== "dev") connectDB();
 
-app.set('view engine', 'pug')
+app.set("view engine", "pug");
 
 // Cors settings
 app.use(credentials);
@@ -29,17 +30,17 @@ app.use(cors(corsOptions));
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true })); // for form data (encoded data)
 app.use(express.json()); // for json data
-app.use('/', express.static(path.join(__dirname, 'public')));
+app.use("/", express.static(path.join(__dirname, "public")));
 
 //routes
-app.use("/", require('./routes/root.js'));
-app.use("/subscribe", require('./routes/subscribe.js'));
+app.use("/", require("./routes/root.js"));
+app.use("/subscribe", require("./routes/subscribe.js"));
 app.use("/api/shootEmail", require("./routes/api/shootEmail"));
 
 // error handler
 app.use(errorHandler);
 
-if (env !== "dev") {
+if (envConfig.envName !== "dev") {
   mongoose.connection.once("open", () => {
     console.log("Connected to mongoDB");
     app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
