98b2f5da56f32d36c17707b820f3718a5829b064
thesixpathguy <thesixpathguy@gmail.com>
2023-06-26 14:03:41 +0530

    changed cron

--- services/cronScheduler/cronScheduler.js
+++ services/cronScheduler/cronScheduler.js
@@ -2,7 +2,7 @@ const CronJob = require("cron").CronJob;
 const axios = require("axios");
 const { logEvents } = require("../../middleware/logEvents");
 
-const cron = "0 15 9 ? * *"; // every day at 9:15 AM
+const cron = "15 9 * * * // every day at 9:15 AM
 
 var cronJob = new CronJob(
   cron,
